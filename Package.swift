0b24b1ac8d0eca4db403216294e47d68cd13fd76
Marco Pagliari <marco.pagliari@adevinta.com>
2023-06-28 12:36:03 +0200

    Fix

--- Package.swift
+++ Package.swift
@@ -1,19 +1,22 @@
-// swift-tools-version:5.8
+// swift-tools-version:5.0
 // Managed by ice
 
 import PackageDescription
 
 let package = Package(
     name: "Shout",
+    platforms: [
+        .macOS(.v10_10)
+    ],
     products: [
         .library(name: "Shout", targets: ["Shout"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Subito-it/BlueSocket", revision: "c7b55a7"),
-        .package(url: "https://github.com/jakeheis/CSSH", from: "1.0.3"),
+       .package(url: "https://github.com/Subito-it/BlueSocket", .branch("mendoza/stable")),
     ],
     targets: [
-        .target(name: "Shout", dependencies: ["Socket"]),
+        .systemLibrary(name: "CSSH", pkgConfig: "libssh2", providers: [.brew(["libssh2","openssl"])]),
+        .target(name: "Shout", dependencies: ["CSSH", "Socket"]),
         .testTarget(name: "ShoutTests", dependencies: ["Shout"]),
     ]
 )
